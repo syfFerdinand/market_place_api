dc9812b4596cd10b9753a5f4a3d8cd22535e5663
xmanferdinand <xmanferdinand@gmail.com>
2022-05-30 10:36:19 +0000

    Adds show action to the users controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # root "articles#index"
   namespace  :api, defaults: {format: :json} do
     namespace :v1 do
-      
+      resources :users, only: [:show]
     end
   end
 
