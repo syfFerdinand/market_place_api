2be0bfa2c3bd8a3b98772c9257f968d5f3fb4cc3
xmanferdinand <xmanferdinand@gmail.com>
2022-06-07 16:30:26 +0000

    Associates products and orders with a placements model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,8 @@
 class Product < ApplicationRecord
   
-
+  
+  has_many :placements, dependent: :destroy
+  has_many :orders, through: :placements
   validates :title, :user_id, presence: true
   validates :price, numericality: {
     greater_than_or_equal_to: 0
