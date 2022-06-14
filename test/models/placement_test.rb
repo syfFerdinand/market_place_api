7ae3920a76c32e880bdce4448e5e8aed575ad156
xmanferdinand <xmanferdinand@gmail.com>
2022-06-14 11:23:12 +0000

    Decreases the product quantity by the placement quantity

--- test/models/placement_test.rb
+++ test/models/placement_test.rb
@@ -4,4 +4,16 @@ class PlacementTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  setup do
+    @placement = placement(:one)
+  end
+
+  test 'decreases the product quantity by the placement quantity' do
+    product = @placement.product
+
+    assert_difference('product.quantity', -@placement.quantity) do
+      @placement.decrement_product_quantity!
+    end
+  end
 end
