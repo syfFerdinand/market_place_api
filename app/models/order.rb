d2314df57fee1145784826c5d1558ac1a63513b2
xmanferdinand <xmanferdinand@gmail.com>
2022-06-14 10:05:53 +0000

    Allows the order to be placed along with product quantity

--- app/models/order.rb
+++ app/models/order.rb
@@ -12,4 +12,11 @@ class Order < ApplicationRecord
     self.total = self.products.map(&:price).sum 
   end
   
+  def build_placements_with_product_ids_and_quantities(product_ids_quantities)
+      product_ids_quantities.each do |product_id_and_quantity|
+        placement  = placements.build(product_id: product_id_and_quantity[:product_id])
+        yield placement if block_given?
+      end
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_07_161918) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_14_083848) do
   create_table "orders", force: :cascade do |t|
     t.integer "user_id", null: false
     t.decimal "total"
@@ -35,6 +35,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_07_161918) do
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "quantity", default: 0
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
